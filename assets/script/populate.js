7ef925cf791a6b833d3aa701497c68a7473897c4
yusalie <71071393+yusalie@users.noreply.github.com>
2021-06-03 19:17:29 +0300

    Update populate.js

--- assets/script/populate.js
+++ assets/script/populate.js
@@ -20,7 +20,7 @@ function populate() {
         mode: "cors",
         headers: {
            'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Access-Control-Allow-Origin' : "*"
+            'Access-Control-Allow-Origin' : '*'
         },
       })
     //turns resp into JavaScript object
